3996173b48ddba712c232305d77fd72dceeb90ee
Rishabh Thakur <Rishabhthakur388@gmail.com>
2025-07-08 19:36:58 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+require('dotenv').config();
+const dbconnect = require('./config/config');
+const morgan = require('morgan');
 const PORT = 3005;
 dbconnect();
+
+//routes
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.use('/data', require('./routes/userdatasRoutes'));
+app.use('/data', require('./routes/addCategoriesRoutes'));
+// app.use('/', require('./routes/stripeRoute'));
+
 // Middleware to log every request
 app.use((req, res, next) => {
   console.log(`Ping received at ${new Date().toLocaleString()} from ${req.ip}`);
@@ -15,5 +28,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on ${PORT}`);
 });
