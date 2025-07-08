c61349b296218b6bda507b3dc5c0347bcc4d3912
Rishabh Thakur <Rishabhthakur388@gmail.com>
2025-07-08 19:09:18 +0530

    update

--- server.js
+++ server.js
@@ -1,94 +1,19 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-require('dotenv').config();
-const dbconnect = require('./config/config');
-const morgan = require('morgan');
-dbconnect();
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(cors());
-app.use('/data', require('./routes/userdatasRoutes'));
-app.use('/data', require('./routes/addCategoriesRoutes'));
-app.use('/', require('./routes/stripeRoute'));
+const PORT = 3005;
 
-const port = process.env.PORT || 3005
-app.listen(port, () => {
-    console.log(`Port is listning on ${port}`);
+// Middleware to log every request
+app.use((req, res, next) => {
+  console.log(`Ping received at ${new Date().toLocaleString()} from ${req.ip}`);
+  next();
 });
 
+// Default route
+app.get('/', (req, res) => {
+  res.send('👋 Ping received!');
+});
 
-// const a = [1,24,5,6,4]
-
-// const a = 10
-//  for(let i=1; i<=a;i++){
-//     const r = i*a
-//     console.log(r);
-// }
-
-// const b = [10];
-// const result = b.reduce(num => num * b);
-// console.log(result);
-
-// const a = 10;
-// const numbers = Array.from({length: a}, (_, index) => index + 1);
-// const result = numbers.map(num => num * a);
-// console.log(result);
-
-
-// const oldArray = [1,23,4,5,6,7]
-
-// const newArr = oldArray.map(doc=>{ return doc-1})
-// console.log(newArr);
-
-// const oldArray = [1, 23, 4, 5, 6, 7];
-
-// const newArr = oldArray.map(doc => doc * 2); // Added the return statement
-// console.log(newArr);
-
-
-// const red = [1,2,3,4,5]
-// const sum = red.reduce((acc,currentValue)=>{return acc+currentValue})
-// console.log(sum);
-
-// let arr = [ [1, 2, 3],
-//             [4, 5, 6],
-//             [7, 8, 9], ];
-
-// function diagonalDifference(arr) {
-//     let leftSum = 0;
-//     let rightSum = 0;
-
-//     for (let i = 0; i < arr.length; i++) {
-//          let b =leftSum += arr[i][i]
-//         let c = rightSum += arr[arr.length - 1 - i][i]
-//         // console.log( b, 'left')
-//         // console.log( c, "right")
-//     }
-
-//     return Math.abs(leftSum - rightSum)
-// }+
-
-// console.log(diagonalDifference(arr));
-
-// const n = 6;
-// for (let i = 1; i <= n; i++) {
-//   let line = '';
-//   for (let j = 0; j < i; j++) {
-//     line += '*';
-//   }
-//   console.log(line);
-// }
-
-
-// let m = 5;
-// for (let i = m; i >= 1; i--) {
-// 	let str = "* ";
-// 	let space = ' ';
-// 	console.log(space.repeat(m-i)+str.repeat(i));
-// }
-
-
-// const arr = ['A', 'N', 'U'];
-// arr[10] = 10;
-// console.log(arr.length);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
